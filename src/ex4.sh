eed2c07cb7279691ca2a10b9296d41b2bf7868c3
Facundo Martinez <facu@spect.ro>
2016-11-16 16:54:25 -0300

    master: 	* Adding patmatmotifs report 	* Fixing script and hardcoding frame size to match the one we are looking at

--- src/ex4.sh
+++ src/ex4.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-getorf -sequence $1 -outseq intermediate.orf
+getorf -sequence $1 -outseq intermediate.orf -minsize 600
 patmatmotifs -sequence intermediate.orf -outfile $2
 rm intermediate.orf
